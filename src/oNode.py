f723d1fe250ea8a1ff187dbd718f6dbeca76a183
Gonçalo Pereira <gteixeira.pereira@gmail.com>
2022-11-06 17:28:33 +0000

    uso de dicionarios inves de tuplos,quando um nó se liga passa porta de atendimento, bootstrapper regista em memória na routing table

--- src/oNode.py
+++ src/oNode.py
@@ -2,21 +2,31 @@ import socket
 from threading import Thread
 import sys
 import os
+import pickle
 from routing import *
 
-def processing():
-    print("Recebi msg")
+listening_port = 0
 
-def processing_active_onode(bytesAddressPair):
+def processing():
+    print("Recebi msg xdxdxd ")
+
+def activeNode_handler(bytesAddressPair):
     ip = bytesAddressPair[1][0]
     msg = bytesAddressPair[0]
-    print(ip)
+
+    # Ir buscar os vizinhos do nodo que se está a ativar
     neighbours = rt.get_neighbours(ip)
-    bytesToSend = str.encode(str(neighbours))
+    # Ativar o nodo passando a porta que este está a atender
+    rt.active_node(ip,int(msg))
+
+    # Serialize to send
+    bytesToSend = pickle.dumps(neighbours)
+
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPClientSocket.sendto(bytesToSend, (ip,int(msg)))
+    UDPClientSocket.sendto(bytesToSend,(ip,int(msg)))
+
 # Server listening
-def service():
+def service_node():
    bufferSize = 1024
 
    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -24,8 +34,10 @@ def service():
    UDPServerSocket.bind(('',0))
 
    port = UDPServerSocket.getsockname()[1]
-   
+
+   # O nodo tem que mandar uma menssagem de registo
    msg = str(port)
+
    bytesToSend = str.encode(msg)
    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
    UDPClientSocket.sendto(bytesToSend, bootstrapperAddressPort)
@@ -33,14 +45,19 @@ def service():
    while(True):
        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
+
+
        thread = Thread(target=processing)
        thread.start()
 
        message = bytesAddressPair[0]
 
+       data = pickle.loads(message)
+
+
        address = bytesAddressPair[1]
 
-       clientMsg = "Message from Client:{}".format(message)
+       clientMsg = "Message from Client:{}".format(str(data))
        clientIP  = "Client IP Address:{}".format(address)
 
        print(clientMsg)
@@ -55,10 +72,11 @@ def service_bootstrapper():
 
    UDPServerSocket.bind((UDPServerSocket.getsockname()[0],5555))
    print(UDPServerSocket.getsockname()[0])
+
    while(True):
        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
-       thread = Thread(target=processing_active_onode,args=[bytesAddressPair])
+       thread = Thread(target=activeNode_handler,args=[bytesAddressPair])
        thread.start()
 
        message = bytesAddressPair[0]
@@ -75,34 +93,36 @@ def service_bootstrapper():
 if __name__ == "__main__":
 
     args = sys.argv[1:]
-    n_args = len(args) 
+    n_args = len(args)
     client = False
     bootstrapper = False
     if n_args==1:
         # Adicionar novo nodo à overlay:
-        # eNode <bootstrapper_ip>
+        # oNode <bootstrapper_adress>
         bootstrapperAddressPort = (args[0],5555)
         client = True
     elif n_args==2:
         # Iniciar bootstrapper:
-        # eNode -bs <config_file>
+        # oNode -bs <config_file>
         if args[0]=="-bs":
             rt = RoutingTable()
             rt.load(args[1])
             #rt.display()
             bootstrapper = True
-        else: print("ERROR")
+        elif args[0]=="-c":
+            # Executar como cliente:
+            # oNode -c <bootstrapper_adress>
+            print("Client running")
+        else:
+             print("ERROR")
 
     else: print("ERROR")
-    
-    
 
     # Listening
     if(bootstrapper):
+
         thread = Thread(target=service_bootstrapper)
         thread.start()
-    else: 
-        thread = Thread(target=service)
+    else:
+        thread = Thread(target=service_node)
         thread.start()
-
-
--- src/routing.py
+++ src/routing.py
@@ -6,13 +6,30 @@ class RoutingTable:
         self.groups = []
 
     @classmethod
-    def add_group(self,node,groupList):
-        self.groups.append((node,groupList))
+    def add_group(self,ip,groupList):
+        self.groups.append({ "node_ip" : ip ,"port" : -1,"neighbours" : groupList})
+
+    def get_port(self,ip):
+        for group in self.groups:
+            if group["node_ip"] == ip:
+                return group["port"]
 
     def get_neighbours(self,ip):
-        for (node,group) in self.groups:
-            if node == ip:
-                return group
+        neighbours_list = []
+        for group in self.groups:
+            if group["node_ip"] == ip:
+                for entry in group["neighbours"]:
+                    neighbours_list.append({ "node_ip" : entry, "port" : self.get_port(entry)})
+
+        return neighbours_list
+
+
+
+    def active_node(self,ip,listening_port):
+        for group in self.groups:
+            if group["node_ip"]==ip:
+                group["port"] = listening_port
+
 
     @classmethod
     def load(self,configFile):
@@ -39,7 +56,7 @@ class RoutingTable:
 
     @classmethod
     def display(self):
-        for (node,group) in self.groups:
-            print("VIZINHOS DE " + node + ":")
-            for entry in group:
+        for group in  self.groups:
+            print("VIZINHOS DE " + group["node_ip"] + ":")
+            for entry in group["neighbours"]:
                 print(entry)
